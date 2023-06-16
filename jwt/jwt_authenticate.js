2530268a18c9a36a3da48e89a2de4ea2ac9162d1
dianna1234 <01234dianna@gmail.com>
2023-06-16 23:19:19 +0400

    add authentication for user and admin

--- jwt/jwt_authenticate.js
+++ jwt/jwt_authenticate.js
@@ -1,46 +1,44 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const SECRET = process.env.SECRET;
 
-function admin_authenticate(req,res,next){
-    try{
-        const token = req.headers.authorization;
+function admin_authenticate(req, res, next) {
+  try {
+    const token = req.headers.authorization;
 
-        if(token === null){
-            res.status(401).json({message:'Not Allowed'})
-        }
-        jwt.verify(token, SECRET, (err,user)=>{
-            if(err){
-                res.status(403).json({message:'Access is forbidden'})
-            }else if(token && user.role === 1){
-                next();
-            }
-        })
-    }catch{
-        res.status(500).json({message:'Server Problem'})
+    if (token === null) {
+      res.status(401).json({ message: "Not Allowed" });
     }
-   
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        res.status(403).json({ message: "Access is forbidden" });
+      } else if (token && user.role === 1) {
+        next();
+      }
+    });
+  } catch {
+    res.status(500).json({ message: "Server Problem" });
+  }
 }
 
-function user_authenticate(req,res,next){
-    try{
-        const token = req.headers.authorization;
+function user_authenticate(req, res, next) {
+  try {
+    const token = req.headers.authorization;
 
-        if(token === null){
-            res.status(401).json({message:'Not Allowed'})
-        }
-        jwt.verify(token, SECRET, (err,user)=>{
-            if(err){
-                res.status(403).json({message:'Access is forbidden'})
-            }else if(token && user.role === 0){
-                next();
-            }
-        })
-    }catch{
-        res.status(500).json({message:'Server Problem'})
+    if (token === null) {
+      res.status(401).json({ message: "Not Allowed" });
     }
-   
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        res.status(403).json({ message: "Access is forbidden" });
+      } else if (token && user.role === 0) {
+        next();
+      }
+    });
+  } catch {
+    res.status(500).json({ message: "Server Problem" });
+  }
 }
 
-module.exports = {admin_authenticate, user_authenticate}
+module.exports = { admin_authenticate, user_authenticate };
