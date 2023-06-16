6e445eaf4586966291d5cf66e78400bbe86fcde4
dianna1234 <01234dianna@gmail.com>
2023-06-16 23:53:59 +0400

    when order բե done delete cartitem and update product quantity

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -1,9 +1,8 @@
-const { Order } = require('../models');
-const { CartItem } = require('../models');
-const { Product } = require('../models');
+const { Order } = require("../models");
+const { CartItem } = require("../models");
+const { Product } = require("../models");
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripe = require('stripe')(stripeSecretKey);
-
+const stripe = require("stripe")(stripeSecretKey);
 
 async function createOrder(req, res) {
   try {
@@ -15,7 +14,9 @@ async function createOrder(req, res) {
       products,
     });
 
-    const cartItems = await CartItem.findAll({ where: { productId: products.map(p => p.productId) } });
+    const cartItems = await CartItem.findAll({
+      where: { productId: products.map((p) => p.productId) },
+    });
 
     for (const cartItem of cartItems) {
       const matchingProduct = await Product.findByPk(cartItem.productId);
@@ -27,7 +28,10 @@ async function createOrder(req, res) {
         updatedQuantity = 0;
       }
 
-      await Product.update({ quantity: updatedQuantity }, { where: { id: matchingProduct.id } });
+      await Product.update(
+        { quantity: updatedQuantity },
+        { where: { id: matchingProduct.id } }
+      );
     }
 
     await CartItem.destroy({ where: { cartId } });
@@ -35,10 +39,9 @@ async function createOrder(req, res) {
     res.status(201).json(order);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
-
 
 async function getAllOrders(req, res) {
   try {
@@ -46,68 +49,75 @@ async function getAllOrders(req, res) {
     res.json(orders);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
 
 async function getOrderById(req, res) {
-  const { orderId } = req.params;
+  const { id } = req.params;
   try {
-    const order = await Order.findOne({where:orderId});
+    const order = await Order.findOne({ where: { cartId: id } });
     if (order) {
       res.json(order);
     } else {
-      res.status(404).json({ message: 'Order not found' });
+      res.status(404).json({ message: "Order not found" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
 
-async function orederPayment(req,res){
+async function orederPayment(req, res) {
   const { token, cartId, products } = req.body;
 
   try {
-    const cartItems = await CartItem.findAll({ where: { productId: products.map(p => p.productId) }, include:Product });
+    const cartItems = await CartItem.findAll({
+      where: { productId: products.map((p) => p.productId) },
+      include: Product,
+    });
 
     const total = cartItems.reduce((acc, item) => acc + item.Product.price, 0);
 
-   stripe.customers
-    .create({
-      email: token.email,
-      source: token.id,
-    })
-    .then((customer) =>
-      stripe.charges.create({
-        amount: total * 100,
-        currency: "usd",
-        customer: customer.id,
+    stripe.customers
+      .create({
+        email: token.email,
+        source: token.id,
       })
-    ).then(async(res)=>{
-      await CartItem.destroy({ where: { cartId } });
-    })
-     await Order.create({
+      .then((customer) =>
+        stripe.charges.create({
+          amount: total * 100,
+          currency: "usd",
+          customer: customer.id,
+        })
+      )
+      .then(async (res) => {
+        await CartItem.destroy({ where: { cartId } });
+      });
+    await Order.create({
       cartId,
       total,
       products,
     });
-  
+
     for (const cartItem of cartItems) {
       const matchingProduct = await Product.findByPk(cartItem.productId);
       const { quantity } = cartItem;
 
       let updatedQuantity = matchingProduct.quantity - quantity;
 
-      if (updatedQuantity < 0) {
+      if (updatedQuantity < 1) {
         updatedQuantity = 0;
       }
 
-      await Product.update({ quantity: updatedQuantity }, { where: { id: matchingProduct.id } });
+      await Product.update(
+        { quantity: updatedQuantity },
+        { where: { id: matchingProduct.id } }
+      );
     }
   } catch (error) {
-    console.error('Error creating payment intent:', error);
-    res.status(500).json({ error: 'Failed to create payment intent' });
+    console.error("Error creating payment intent:", error);
+    res.status(500).json({ error: "Failed to create payment intent" });
   }
 }
 
@@ -115,5 +125,5 @@ module.exports = {
   createOrder,
   getAllOrders,
   getOrderById,
-  orederPayment
+  orederPayment,
 };
