69045dd8c2f15957d145e25f700360c8eb0e7e53
dianna1234 <01234dianna@gmail.com>
2023-06-16 23:17:15 +0400

    add all orders and one order route

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,8 @@
 const order_controller = require('../controllers/order_controller');
 
 function order_routes(app){  
-    app.post('/createOrder', order_controller.createOrder);
+    app.get('/orders', order_controller.getAllOrders);
+    app.get('/order/:id', order_controller.getOrderById);
     app.post('/createOrderPayment', order_controller.orederPayment);
 }
 
