c84522887f404e25c2e400e14b99ab272b3686ec
dianna1234 <01234dianna@gmail.com>
2023-06-23 23:54:54 +0400

    changes

--- jwt/jwt_authenticate.js
+++ jwt/jwt_authenticate.js
@@ -27,7 +27,7 @@ function user_authenticate(req, res, next) {
     const token = req.headers.authorization;
 
     if (token === null) {
-      res.status(401).json({ message: "Not Allowed" });
+      res.status(401).json({ message: "Not Allowed!" });
     }
     jwt.verify(token, SECRET, (err, user) => {
       if (err) {
@@ -40,5 +40,82 @@ function user_authenticate(req, res, next) {
     res.status(500).json({ message: "Server Problem" });
   }
 }
+
+
+
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const {Cart} = require('../models');
+
+// const SECRET = process.env.SECRET;
+
+// function admin_authenticate(req, res, next) {
+//   try {
+//     const token = req.headers.authorization;
+
+//     if (!token) {
+//       res.status(401).json({ message: "Not Allowed" });
+//       return;
+//     }
+
+//     jwt.verify(token, SECRET, (err, decoded) => {
+//       if (err) {
+//         res.status(403).json({ message: "Access is forbidden" });
+//         return;
+//       }
+
+//       if (decoded.role !== 1) {
+//         res.status(403).json({ message: "Access is forbidden" });
+//         return;
+//       }
+
+//       // Check if the token belongs to the correct user (optional)
+//       if (decoded.id !== req.params.userId) {
+//         res.status(403).json({ message: "Access is forbidden!" });
+//         return;
+//       }
+
+//       next();
+//     });
+//   } catch {
+//     res.status(500).json({ message: "Server Problem" });
+//   }
+// }
+
+//  function user_authenticate(req, res, next) {
+//   try {
+//     const token = req.headers.authorization;
+//     const {id} = req.params;
+//     console.log(id,'req')
+
+//     if (!token) {
+//       res.status(401).json({ message: "Not Allowed" });
+//       return;
+//     }
+
+//     jwt.verify(token, SECRET, async(err, user) => {
+//       if (err) {
+//         res.status(403).json({ message: "Access is forbidden" });
+//         return;
+//       }
+
+//       if (user.role !== 0) {
+//         res.status(403).json({ message: "Access is forbidden" });
+//         return;
+//       }
+
+//       // const getCart = await Cart.findOne({where:{userId:id}});
+      
+//       // if (!getCart || user.id !== getCart.userId) {
+//       //   res.status(403).json({ message: "You don't have rights" });
+//       //   return;
+//       // }
+
+//       next();
+//     });
+//   } catch {
+//     res.status(500).json({ message: "Server Problem" });
+//   }
+// }
 
 module.exports = { admin_authenticate, user_authenticate };
