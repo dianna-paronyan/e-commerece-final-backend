4a9cb002ec5576b79b783ef72258bf4b9753d7c9
dianna1234 <01234dianna@gmail.com>
2023-06-23 23:54:20 +0400

    add is_verified

--- jwt/jwt_generateAccessToken.js
+++ jwt/jwt_generateAccessToken.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const SECRET = process.env.SECRET;
 
-function generateAccessToken(email, role, id) {
-  return jwt.sign({ email, role, id }, SECRET, { expiresIn: "36000s" });
+function generateAccessToken(email, role, id, is_verified) {
+  return jwt.sign({ email, role, id , is_verified}, SECRET, { expiresIn: "36000s" });
 }
 
 module.exports = { generateAccessToken };
