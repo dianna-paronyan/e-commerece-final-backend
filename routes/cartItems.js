4c829c7e103e64d5ff0dabd8ff2ede8decb92e7d
dianna1234 <01234dianna@gmail.com>
2023-06-23 23:54:01 +0400

    delete unused

--- routes/cartItems.js
+++ routes/cartItems.js
@@ -1,13 +1,13 @@
 const cartItems_controller = require("../controllers/cartItems_conroller");
+const { user_authenticate } = require("../jwt/jwt_authenticate");
 
 function cartItems_routes(app) {
-  app.get("/cartItems", cartItems_controller.allCartItems);
-  app.get("/cartItem/:id", cartItems_controller.getCartItem);
-  app.put("/updateCartItem/:id", cartItems_controller.updateCartItem);
-  app.delete("/deleteCartItem/:id", cartItems_controller.deleteCartItem);
-  app.post("/cartCartItem", cartItems_controller.createCartAndCartItem);
+  app.get("/cartItems",user_authenticate, cartItems_controller.allCartItems);
+  app.get("/cartItem/:id", user_authenticate, cartItems_controller.getCartItem);
+  app.delete("/deleteCartItem/:id",user_authenticate, cartItems_controller.deleteCartItem);
+  app.post("/cartCartItem",user_authenticate, cartItems_controller.createCartAndCartItem);
   app.put("/cartItem/increment/:id", cartItems_controller.incrementCartItem);
-  app.put("/cartItem/decrement/:id", cartItems_controller.decrementCartItem);
+  app.put("/cartItem/decrement/:id",user_authenticate, cartItems_controller.decrementCartItem);
 }
 
 module.exports = {
