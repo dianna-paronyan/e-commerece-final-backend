c15a24c664b99f2b41a210fbfe3bfe1d816fcce8
dianna1234 <01234dianna@gmail.com>
2023-06-29 18:29:48 +0400

    add user authentication middlware

--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,8 @@ const order_controller = require('../controllers/order_controller');
 const { user_authenticate } = require('../jwt/jwt_authenticate');
 
 function order_routes(app){  
-    app.get('/orders', user_authenticate, order_controller.getAllOrders);
-    app.get('/order/:id', user_authenticate, order_controller.getOrderById);
+    app.get('/orders', order_controller.getAllOrders);
+    app.get('/order/:id', order_controller.getOrderById);
     app.post('/createOrderPayment', user_authenticate, order_controller.orederPayment);
 }
 
